8d38c35e131a6cf234330a23f7fcd62bcba9b4dd
JesDanFors <90110706+JesDanFors@users.noreply.github.com>
2022-02-03 10:45:46 +0100

    Update to camera and start on second implimication

--- 2.5D platformer class/Assets/Scripts/JesperDanielsson/GrapplingHook/Implementation_2_SwingGrapple.cs
+++ 2.5D platformer class/Assets/Scripts/JesperDanielsson/GrapplingHook/Implementation_2_SwingGrapple.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Implementation_2_SwingGrapple : MonoBehaviour
-{
-    
+public class Implementation_2_SwingGrapple : MonoBehaviour{
+    [SerializeField] float maxDistance = 10f;
+    [SerializeField] float swingSpeed = 1f;
 }

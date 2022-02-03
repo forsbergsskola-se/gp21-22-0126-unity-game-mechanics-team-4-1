e4d0a4dcfb9e7175fa8e8e50588df733a67c0ee0
Felix K <felix.kjellberg@student.forsbergsskola.se>
2022-02-03 17:02:42 +0100

    Started working on Imploding Mine

--- 2.5D platformer class/Assets/Scripts/FelixKjellberg/Explosion/Implementation_2_Implosion.cs
+++ 2.5D platformer class/Assets/Scripts/FelixKjellberg/Explosion/Implementation_2_Implosion.cs
@@ -1,22 +1,19 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Implementation_1_Explosion : MonoBehaviour{
-    [SerializeField] float explosionForce = 800f;
+public class Implementation_2_Implosion : MonoBehaviour
+{
+    [SerializeField] float implosionForce = 10f;
     [SerializeField] GameObject particleSystem;
     [SerializeField] GameObject mineMesh;
-    
-    
-
     void OnTriggerEnter(Collider other){
         var targetRigidbody = other.GetComponent<Rigidbody>();
         if (targetRigidbody == null)
             return;
         
-        var targetDirection = targetRigidbody.transform.position.normalized - this.transform.position.normalized;
-        targetRigidbody.AddForce(targetDirection * explosionForce, ForceMode.Impulse);
+        var targetDirection = this.transform.position.normalized - targetRigidbody.transform.position.normalized;
+        targetRigidbody.AddForce(targetDirection * implosionForce, ForceMode.Acceleration);
         particleSystem.SetActive(true);
         mineMesh.SetActive(false);
         GetComponent<Collider>().enabled = false;

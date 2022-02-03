9eea9cb96982ad09448e1e22492427d0cbc67bda
Felix K <felix.kjellberg@student.forsbergsskola.se>
2022-02-03 16:38:49 +0100

    Worked on exploding mine

--- 2.5D platformer class/Assets/Scripts/FelixKjellberg/Explosion/Implementation_1_Explosion.cs
+++ 2.5D platformer class/Assets/Scripts/FelixKjellberg/Explosion/Implementation_1_Explosion.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 
 public class Implementation_1_Explosion : MonoBehaviour{
     [SerializeField] float explosionForce = 800f;
+    [SerializeField] GameObject particleSystem;
+    [SerializeField] float chargeUpTime = 1f;
+    [SerializeField] GameObject mineMesh;
+    
+    
 
     void OnTriggerEnter(Collider other){
         var targetRigidbody = other.GetComponent<Rigidbody>();
@@ -13,5 +18,8 @@ public class Implementation_1_Explosion : MonoBehaviour{
         
         var targetDirection = targetRigidbody.transform.position.normalized - this.transform.position.normalized;
         targetRigidbody.AddForce(targetDirection * explosionForce, ForceMode.Impulse);
+        particleSystem.SetActive(true);
+        mineMesh.SetActive(false);
+        GetComponent<Collider>().enabled = false;
     }
 }

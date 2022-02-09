919dde6dcc8e18630b373c70f0f4e1cc43ef7a4b
Nadia Dahlberg <nadiadahlberg@outlook.com>
2022-02-10 00:51:11 +0100

    Implementing Dash 1 to Main Scene

--- 2.5D platformer class/Assets/Scripts/NadiaDahlberg/Dash/Implementation_1_/Implementation_1_Dash.cs
+++ 2.5D platformer class/Assets/Scripts/NadiaDahlberg/Dash/Implementation_1_/Implementation_1_Dash.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Dash : MonoBehaviour
+public class Implementation_1_Dash : MonoBehaviour
 {
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask ground;
